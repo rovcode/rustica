e3eb2bcf3a8bc459ec9520116b2f4c5741a89901
Rony Velásquez <velasquezcordovar@gmail.com>
2022-05-18 15:57:42 -0500

    Cambios

--- lib/ui/view/pantalla_registro.dart
+++ lib/ui/view/pantalla_registro.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 class Registro extends StatefulWidget{
-
+ @override
+  RegistroState createState() => RegistroState();
 }
 
-class RegistroUseState extends State<Registro>{
+class RegistroState extends State<Registro>{
    @override
    Widget build(BuildContext context){
-     return
-   }
+     return  Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ));
+  }
+}
