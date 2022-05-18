f46d4e21909786d1aae9a1a408b264024e6875b2
Rony Velásquez <velasquezcordovar@gmail.com>
2022-05-18 15:55:27 -0500

    Avance registro

---
+++ lib/ui/view/pantalla_registro.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Registro extends StatefulWidget{
+
+}
+
+class RegistroUseState extends State<Registro>{
+   @override
+   Widget build(BuildContext context){
+     return
+   }
